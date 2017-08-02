0661518702d3b801dcb1e74bc0e40aa6f83ebb75
Amon Gathoka <waitaamon@gmail.com>
2017-08-02 10:00:54 +0300

    added candidates controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 
-use App\User;
+
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Tymon\JWTAuth\JWTAuth;
---
+++ database/migrations/2017_08_02_064856_create_candidates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid')->unique();
+            $table->string('name');
+            $table->string('position');
+            $table->string('gender');
+            $table->string('idNumber')->nullable();
+            $table->string('phoneNumber')->nullable();
+            $table->string('email')->nullable();
+            $table->string('nick_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
